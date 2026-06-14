c75dff077308259cf955988f03ee04473cc7aef3
prakhar-gamer <prakhargupta0402@gmail.com>
2026-06-14 14:55:59 -0700

    Made Movement Impulse/physics based

--- scripts/map.gd
+++ scripts/map.gd
@@ -3,28 +3,44 @@ extends CharacterBody2D
 @onready var sprite = $Sprite2D
 var x_direction = true
 
-var speed = 5000
-var movement = Vector2.ZERO
+@export var scroll_impulse: float = 100
+@export var friction: float = 0.0
+@export var max_speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
-	movement = Vector2.ZERO
-	
 	if x_direction:
+		velocity.y = 0
 		if Input.is_action_just_pressed("scroll_up"):
-			movement.x = 1
+			velocity.x += scroll_impulse
 		elif Input.is_action_just_pressed("scroll_down"):
-			movement.x = -1
-	else:
-		if Input.is_action_just_pressed("scroll_up"):
-			movement.y = 1
-		elif Input.is_action_just_pressed("scroll_down"):
-			movement.y = -1
+			velocity.x -= scroll_impulse
 		
-	velocity = speed * movement
+		#(from, to, weight)
+		velocity.x = lerp(velocity.x, 0.0, friction * delta)
+		
+		if abs(velocity.x) < 5:
+			velocity.x = 0
+			
+		velocity.x = clampf(velocity.x, -max_speed, max_speed)
+	else:
+		
+		velocity.x = 0
+		if Input.is_action_just_pressed("scroll_up"):
+			velocity.y += scroll_impulse
+		elif Input.is_action_just_pressed("scroll_down"):
+			velocity.y -= scroll_impulse
+			
+		velocity.y = lerp(velocity.y, 0.0, friction * delta)
+		
+		if abs(velocity.y) < 5:
+			velocity.y = 0
+			
+		velocity.y = clampf(velocity.y, -max_speed, max_speed)
+		
 	move_and_slide()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
